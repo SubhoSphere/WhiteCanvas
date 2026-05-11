217e187f14ea22c0ad2ba5dd60fafa4155427f13
SubhoSphere <subhosphere@gmail.com>
2026-05-11 16:15:53 +0530

    feat: create main application layout with navigation and footer components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
     @yield('meta')
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
